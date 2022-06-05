6872f79c7bbf697a0762852413c1ac1cc18aa534
Aliaksei Barysevich <barysevichaliaksei@yandex.by>
2022-06-05 23:31:23 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_occupation');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__occupation');
+
+editButton.addEventListener('click', function () {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+});
+
+let closeButton = document.querySelector('.popup__close-button');
+
+closeButton.addEventListener('click', function () {
+    popup.classList.remove('popup_opened');
+});
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.remove('popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
